b34530e59b86f1db51a0c32d3fc285e362b372ab
郑松 <zheng.song@menpuji.com>
2017-03-30 13:46:13 +0800

    init

---
+++ app/src/main/java/com/victorai60/finishaffinity/Main2Activity.java
@@ -0,0 +1,18 @@
+package com.victorai60.finishaffinity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+    public void onClick(View v) {
+        finishAffinity();
+    }
+}
---
+++ app/src/main/java/com/victorai60/finishaffinity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.victorai60.finishaffinity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View v) {
+        startActivity(new Intent(this, Main2Activity.class));
+    }
+}
